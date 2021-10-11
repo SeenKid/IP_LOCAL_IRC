cc82b9bf523f3a964d98828968bf4214b86798bd
SeenKid <30565751+SeenKid@users.noreply.github.com>
2021-10-11 18:20:12 +0200

    test p2p

---
+++ Peer2Peer.py
@@ -0,0 +1,24 @@
+import time, socket, sys
+ 
+socket_server = socket.socket()
+server_host = socket.gethostname()
+ip = socket.gethostbyname(server_host)
+sport = 8080
+
+print('Made by SeenKid#0001')
+print('Ton ip est : ',ip)
+server_host = input('IP de l\'ami :')
+name = input('Nom de l\'ami: ')
+
+socket_server.connect((server_host, sport))
+ 
+socket_server.send(name.encode())
+server_name = socket_server.recv(1024)
+server_name = server_name.decode()
+ 
+print(server_name,' a rejoint le chat...')
+while True:
+    message = (socket_server.recv(1024)).decode()
+    print(server_name, ":", message)
+    message = input("Vous : ")
+    socket_server.send(message.encode())  
